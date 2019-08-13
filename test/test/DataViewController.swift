e8a6d250118c4061b4dbe84bde0622b81f6bdd72
Mina Morcos <mmorcos@Minas-MacBook-Pro.local>
2019-08-12 20:30:19 -0400

    newb fb login button working

---
+++ test/test/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  test
+//
+//  Created by Mina Morcos on 8/8/19.
+//  Copyright © 2019 Mina Morcos. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
